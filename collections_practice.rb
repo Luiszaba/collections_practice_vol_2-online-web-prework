522341193a1f6f4cc11e4eb0d7fce0f1374d6999
Luis Zabala <luis.zabala076@gmail.com>
2019-01-11 01:37:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
 # your code goes here
+
+def begins_with_r(array)
+  array.all? do |words|
+    words[0] == 'r'
+  end
+end
+
+def contain_a(array)
+  array.select do |word|
+  word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |word|
+    word[0..1] == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |word|
+    word.instance_of?(String) == false
+  end
+end
+
+def count_elements(array)
+  array.uniq.each |word|
+end

3b2c1747d416cc8b07c5f63b1e3955812bc0acee
Luis Zabala <luis.zabala076@gmail.com>
2019-01-11 01:46:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,5 +25,5 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-count_elements(array).collect? {|elements| elements << array}
+count_elements(array).collect {|elements| elements << array}
 end
